e71fa27d505308fe57916411b4f2215a56b35bc6
MariusNaydenov <marius.naydenov1994@gmail.com>
2024-02-27 15:29:24 +0200

    Add SignIn View

--- Quizzy/src/App.jsx
+++ Quizzy/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import SignUp from "./Views/SignUp/SignUp";
+import SignIn from "./Views/SignUp/SignIn/SignIn";
 
 function App() {
   return (
     <>
       <Header />
-      <SignUp />
+      {/* <SignUp /> */}
+      <SignIn />
     </>
   );
 }

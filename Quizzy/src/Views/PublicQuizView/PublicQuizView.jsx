6ce1f3bbe0e4d60935959a8efdca4e55ccf9321e
MariusNaydenov <marius.naydenov1994@gmail.com>
2024-03-03 15:59:31 +0200

    Adding taking quiz view and functionality

--- Quizzy/src/Views/PublicQuizView/PublicQuizView.jsx
+++ Quizzy/src/Views/PublicQuizView/PublicQuizView.jsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { quizzesData } from "../PublicQuizzes/PublicQuizzes";
-import { Box, Typography, Button, Pagination } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Button,
+  Pagination,
+  Grid,
+  Paper,
+} from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
+
 const theme = createTheme({
   palette: {
     primary: {
       main: "rgba(3,165,251)",
-      contrastText: '#ffffff'
+      contrastText: "#ffffff",
     },
   },
 });
 
-// question.question, question.options.map , question.answer
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const objQuestions = {
   id: 1,
@@ -20,25 +35,29 @@ const objQuestions = {
     {
       question: "What does HTML stand for?",
       options: [
-        { option: "Hyper Text Markup Language" },
-        { option: "Hyperlinks and Text Markup Language" },
-        { option: "Home Tool Markup Language" },
+        { option: "Hyper Text Markup Language", value: 1 },
+        { option: "Hyperlinks and Text Markup Language", value: 2 },
+        { option: "Home Tool Markup Language", value: 3 },
       ],
       answer: 1,
     },
     {
       question: "Choose the correct HTML element for the largest heading?",
       options: [
-        { option: "<heading>" },
-        { option: "<h1>" },
-        { option: "<h6>" },
-        { option: "<head>" },
+        { option: "<heading>", value: 1 },
+        { option: "<h1>", value: 2 },
+        { option: "<h6>", value: 3 },
+        { option: "<head>", value: 4 },
       ],
       answer: 2,
     },
     {
       question: "What is the correct HTML element for inserting a line break?",
-      options: [{ option: "<lb>" }, { option: "<br>" }, { option: "<break>" }],
+      options: [
+        { option: "<lb>", value: 1 },
+        { option: "<br>", value: 2 },
+        { option: "<break>", value: 3 },
+      ],
       answer: 3,
     },
   ],
@@ -47,36 +66,119 @@ const objQuestions = {
 const PublicQuizView = () => {
   const { id } = useParams();
   const [index, setIndex] = useState(0);
+  const [page, setPage] = useState(1);
+  const [questionaryView, setQuestionaryView] = useState(true);
+  const [resultView, setResultView] = useState(false);
+  const [selectedItem, setSelectedItem] = useState({});
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [points, setPoits] = useState({});
+  console.log(points);
+  const handleClick = (option, value, answer, page) => {
+    setSelectedItem({ ...selectedItem, [page]: option });
+    if (value === answer) {
+      setPoits({ ...points, [page]: 1 });
+    } else {
+      setPoits({ ...points, [page]: 0 });
+    }
+  };
+
+  const handleChange = (event, value) => {
+    setPage(value);
+    setIndex(value - 1);
+  };
+
+  const handleview = () => {
+    setQuestionaryView(false);
+    setResultView(true);
+  };
 
   const question = objQuestions.questions;
   const length = objQuestions.questions.length;
 
   return (
-    <Box
-      sx={{
-        boxShadow: 4,
-        width: "400px",
-        height: "400px",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <span>
-        {index + 1} / {length}
-      </span>
-      <Typography variant="h5">{question[index].question}</Typography>
-      {question[index].options.map((option) => {
-        return (
-          <Typography variant="span" sx={{ cursor: "pointer" }}>
-            {option.option}
+    <>
+      {questionaryView && (
+        <Box
+          sx={{
+            boxShadow: 4,
+            width: "600px",
+            height: "auto",
+            display: "flex",
+            flexDirection: "column",
+            padding: "10px",
+          }}
+        >
+          <span style={{ color: "#394E6A", fontFamily: "Fantasy" }}>
+            Question
+            <span style={{ fontSize: "23px", marginLeft: "7px" }}>
+              {index + 1}
+            </span>{" "}
+            / {length}
+          </span>
+          <Typography
+            style={{ fontFamily: "fantasy", color: "#394E6A" }}
+            variant="h5"
+          >
+            {question[index].question}
           </Typography>
-        );
-      })}
-      <ThemeProvider theme={theme}>
-        <Pagination count={length} color="primary" page={index} />
-      </ThemeProvider>
-      <Button>Submit</Button>
-    </Box>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            {question[index].options.map((option) => {
+              return (
+                <Grid item xs={6}>
+                  <Item
+                    style={{
+                      fontFamily: "Monospace",
+                      cursor: "pointer",
+                      backgroundColor:
+                        selectedItem[page] === option
+                          ? "rgb(3,165,251)"
+                          : "initial",
+                      color:
+                        selectedItem[page] === option
+                          ? "#ffffff"
+                          : theme.palette.text.secondary,
+                    }}
+                    onClick={() =>
+                      handleClick(
+                        option,
+                        option.value,
+                        question[index].answer,
+                        page
+                      )
+                    }
+                  >
+                    {option.option}
+                  </Item>
+                </Grid>
+              );
+            })}
+          </Grid>
+          <ThemeProvider theme={theme}>
+            <Pagination
+              count={length}
+              color="primary"
+              page={page}
+              onChange={handleChange}
+            />
+          </ThemeProvider>
+          <Button
+            style={{
+              backgroundColor: "rgb(3,165,251)",
+              color: "white",
+              width: "130px",
+            }}
+            onClick={handleview}
+          >
+            Submit
+          </Button>
+        </Box>
+      )}
+      {resultView && <Box>HELLO</Box>}
+    </>
   );
 };
 

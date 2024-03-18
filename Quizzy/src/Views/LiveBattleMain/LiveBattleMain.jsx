c180d6693c811e2b88df6f709ba6375a9640d777
MariusNaydenov <marius.naydenov1994@gmail.com>
2024-03-19 01:45:19 +0200

    Add state for users and state to show and hide online users

--- Quizzy/src/Views/LiveBattleMain/LiveBattleMain.jsx
+++ Quizzy/src/Views/LiveBattleMain/LiveBattleMain.jsx
@@ -1,11 +1,35 @@
 import { Box, Button, Typography } from "@mui/material";
 import quizLogo from ".//..//..//Images/quiz-battle-home-pic.jpg";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import AppContext from "../../Context/AppContext";
 import { useState } from "react";
+import { getAllUsers } from "../../services/users.service";
+import UserProfilePic from "../../Components/UserProfilePic/UserProfilePic";
 
 const LiveBattleMain = () => {
+  const [onlineUsers, setOnlineUsers] = useState([]);
   const { userData } = useContext(AppContext);
+  const [showOnlineUsers, setShowOnlineUsers] = useState(false);
+
+  const handleOnlineUsersView = () => {
+    if (showOnlineUsers) {
+      setShowOnlineUsers(false);
+    } else {
+      setShowOnlineUsers(true);
+    }
+  };
+
+  useEffect(() => {
+    getAllUsers()
+      .then((users) => {
+        return users.filter((user) => {
+          return (
+            user.status === "online" && user.username !== userData?.username
+          );
+        });
+      })
+      .then((usersOnline) => setOnlineUsers(usersOnline));
+  }, []);
 
   return (
     <Box
@@ -45,7 +69,14 @@ const LiveBattleMain = () => {
         Welcome to the{" "}
         <span style={{ color: "rgb(3,165,251)" }}>Battle of Brains</span> !
       </Typography>
-      <Box sx={{ display: "flex" }}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "center",
+          gap: "100px",
+        }}
+      >
         <img
           src={quizLogo}
           style={{
@@ -61,6 +92,7 @@ const LiveBattleMain = () => {
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            // marginLeft:'60px'
           }}
         >
           <Typography
@@ -69,7 +101,7 @@ const LiveBattleMain = () => {
               fontFamily: "fantasy",
               fontSize: "40px",
               color: "#394E6A",
-              marginTop: "30px",
+              //   marginTop: "30px",
               textAlign: "center",
             }}
           >
@@ -79,16 +111,45 @@ const LiveBattleMain = () => {
           <Button
             variant="contained"
             sx={{
-              width: "125px",
+              width: "160px",
               height: "40px",
               fontSize: "12px",
               backgroundColor: "rgb(3,165,251)",
               color: "white",
               marginTop: "30px",
+              marginBottom: "30px",
             }}
+            onClick={handleOnlineUsersView}
           >
-            Invite Rival
+            {showOnlineUsers === false ? 'See who is online' : 'Hide online users'}
           </Button>
+          {showOnlineUsers && (
+            <Box sx={{ display: "flex", width: "100%" }}>
+              <Box
+                sx={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(4,1fr)",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "10px",
+                  width: "100%",
+                }}
+              >
+                {onlineUsers.map((user) => {
+                  return (
+                    <Box sx={{ display: "flex", flexDirection: "column" }}>
+                      <UserProfilePic
+                        image={user.image}
+                        width={"48px"}
+                        height={"48px"}
+                      />
+                      <span>{user.firstName}</span>
+                    </Box>
+                  );
+                })}
+              </Box>
+            </Box>
+          )}
         </Box>
       </Box>
     </Box>

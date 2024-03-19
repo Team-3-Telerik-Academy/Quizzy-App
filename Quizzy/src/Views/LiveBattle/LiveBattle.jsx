110de3c418703924e9a3223fc68a0ef1761a7681
MariusNaydenov <marius.naydenov1994@gmail.com>
2024-03-19 20:54:53 +0200

    Add view and functionality to take the battle quiz

--- Quizzy/src/Views/LiveBattle/LiveBattle.jsx
+++ Quizzy/src/Views/LiveBattle/LiveBattle.jsx
@@ -1,4 +1,11 @@
-import { Box, Button, CircularProgress, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Typography,
+  Grid,
+  Paper,
+} from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {
@@ -9,6 +16,25 @@ import {
 } from "../../services/live-battle.services";
 import AppContext from "../../Context/AppContext";
 import { getLiveBattleQuestions } from "../../services/request-service";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "rgba(3,165,251)",
+      contrastText: "#ffffff",
+    },
+  },
+});
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const LiveBattle = () => {
   const { battleId } = useParams();
@@ -17,12 +43,19 @@ const LiveBattle = () => {
   const [quiz, setQuiz] = useState([]);
   const [categoriesView, setCategoriesView] = useState(true);
   const [loading, setLoading] = useState(false);
-
+  const [secondsLeft, setSecondsLeft] = useState(15);
+  const [buttonColor, setButtonColor] = useState("rgb(3,165,251)");
+  const [points, setPoints] = useState({});
+  const [questions, setQuestions] = useState([]);
+  const length = questions?.length;
+  const [selectedItem, setSelectedItem] = useState({});
+  const [index, setIndex] = useState(0);
   const [chosenCategory, setChosenCategory] = useState({});
 
   useEffect(() => {
     getLiveBattleById(battleId).then((data) => {
       setLiveBattle(data);
+      setQuestions(Object.values(data.questions));
       listenToLiveBattle(battleId, (data) => {
         setLiveBattle(data);
       });
@@ -56,6 +89,31 @@ const LiveBattle = () => {
       updateLiveBattle(battleId, "category1", category);
     } else {
       updateLiveBattle(battleId, "category2", category);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSecondsLeft((prevSec) => {
+        if (prevSec > 0) {
+          return prevSec - 1;
+        } else {
+          setIndex((prev) => prev + 1);
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const handleClick = (question, answer, index, questionPoint) => {
+    setSelectedItem({ ...selectedItem, [index]: question });
+    setButtonColor("rgb(3,165,251)");
+
+    if (question === answer) {
+      setPoints({ ...points, [index]: questionPoint });
+    } else {
+      setPoints({ ...points, [index]: 0 });
     }
   };
 
@@ -205,7 +263,132 @@ const LiveBattle = () => {
           {loading && <CircularProgress />}
         </Box>
       ) : (
-        <h1></h1>
+        <>
+          <Box
+            sx={{
+              minWidth: "100%",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              alignItems: "center",
+              position: "relative",
+            }}
+          >
+            <Typography
+              style={{
+                color: "#394E6A",
+                fontFamily: "Fantasy",
+                marginTop: "30px",
+                marginBottom: "5px",
+                fontSize: "25px",
+              }}
+            >
+              {quiz.title} Quiz
+            </Typography>
+
+            <Typography
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                fontFamily: "cursive",
+                color: "#394E6A",
+                fontSize: "17px",
+                minWidth: "45%",
+                marginBottom: "5px",
+              }}
+            >
+              {" "}
+              Time left: {secondsLeft}
+            </Typography>
+
+            <Box
+              sx={{
+                boxShadow: 4,
+                width: "45%",
+                height: "auto",
+                display: "flex",
+                flexDirection: "column",
+                padding: "20px",
+                borderRadius: "10px",
+              }}
+            >
+              <span
+                style={{
+                  color: "#394E6A",
+                  fontFamily: "Fantasy",
+                  marginBottom: "10px",
+                }}
+              >
+                Question
+                <span style={{ fontSize: "23px", marginLeft: "7px" }}>
+                  {index + 1}
+                </span>{" "}
+                / {length}
+              </span>
+              <Typography
+                style={{
+                  fontFamily: "fantasy",
+                  color: "#394E6A",
+                  marginBottom: "20px",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+                variant="h5"
+              >
+                {questions[index]?.title}
+              </Typography>
+              <Grid
+                container
+                rowSpacing={1}
+                columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+              >
+                {questions[index]?.answers?.map((question) => {
+                  return (
+                    <Grid key={question} item xs={6}>
+                      <Item
+                        sx={{
+                          fontFamily: "Monospace",
+                          cursor: "pointer",
+                          backgroundColor:
+                            selectedItem[index] === question
+                              ? "rgb(3,165,251)"
+                              : "initial",
+                          color:
+                            selectedItem[index] === question
+                              ? "#ffffff"
+                              : theme.palette.text.secondary,
+                          "&:hover": {
+                            backgroundColor:
+                              selectedItem[index] === question
+                                ? buttonColor
+                                : "#EBECF0",
+                          },
+                          transition: "transform 0.2s",
+                        }}
+                        onMouseOver={(e) => {
+                          e.currentTarget.style.transform = "scale(1.03)";
+                        }}
+                        onMouseOut={(e) => {
+                          e.currentTarget.style.transform = "scale(1)";
+                        }}
+                        onClick={() =>
+                          handleClick(
+                            question,
+                            questions[index].correctAnswer,
+                            index,
+                            questionPoint
+                          )
+                        }
+                      >
+                        {question}
+                      </Item>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Box>
+          </Box>
+        </>
       )}
     </>
   );
